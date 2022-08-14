0e8985ecdc6753350bff1e53636f126a04b55259
TuanDao2002 <tuan09092002@gmail.com>
2022-08-14 21:53:42 +0700

    first commit

---
+++ RMIT_Roulette/RMIT_RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_RouletteApp.swift
+//  RMIT_Roulette
+//
+//  Created by Tuan Dao on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
